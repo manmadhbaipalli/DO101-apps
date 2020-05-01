0b3afeda08422eab97cbea7fd492aa8d09afe485
manmadhbaipalli <mdrao.b@gmail.com>
2020-05-01 17:06:42 -0400

    env prop

--- version/app.js
+++ version/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const TEST = process.env.TEST;
+const TEST1 = process.env.TEST1;
 app = express();
 
 var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 4 of the app.' + '\n' + TEST;
+    response = 'This is version 4 of the app.' + '\n' + TEST +"TEST1: "+TEST1;
 
     //send the response to the client
     res.send(response);
