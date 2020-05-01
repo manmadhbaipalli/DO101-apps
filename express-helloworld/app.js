91e8453f5b8c2f8bc71c84385d972ff6a75eb04f
manmadhbaipalli <mdrao.b@gmail.com>
2020-05-01 16:12:19 -0400

    adding env

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,12 +1,13 @@
 var express = require('express');
+const TEST = process.env.TEST;
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World!\n' +TEST);
 });
 
 app.get('/manmadh', function (req, res) {
-  res.send('Hello Manmadh!\n');
+  res.send('Hello Manmadh!\n' +TEST);
 });
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
